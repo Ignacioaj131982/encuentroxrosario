c66dc477bc4ae188c33e52616b215286a0b5c310
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2024-02-12 22:16:16 -0300

    Pagina alternativa con seccion novedades

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-console.log("Inicio de script.js");
+console.log ("Inicio de script.js");
 
 
-function activarAnimacion() {
-    var elemento1 = document.querySelector('.encontrarnos');
-    var elemento2 = document.querySelector('.barrioxbarrio')
-    elemento1.classList.toggle('animado');
-    elemento2.classList.toggle('animado');
-}
+
 
 const imagen1 = document.getElementById('imagen1');
 const imagen2 = document.getElementById('imagen2');
@@ -27,7 +22,7 @@ const cargarImagen = (entradas, observador) =>  {
 const observador = new IntersectionObserver(cargarImagen, {
     root: null,
     rootMargin: '0px', //'0px 0px 0px 0px' si queres que se active la funcion con otro margen que no sea el de la pantalla
-    threshold: 1.0 //quiero que ejecute el codigo cuando la imagen esta completa en el viewport
+    //threshold: 1.0 //quiero que ejecute el codigo cuando la imagen esta completa en el viewport
 });
 
 observador.observe(imagen1);
